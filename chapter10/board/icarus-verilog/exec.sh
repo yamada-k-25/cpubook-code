7f24e9afbebdf419e760b6757fd904a6649f67d4
yamada-k-25 <yamada.kazuki.sg@alumni.tsukuba.ac.jp>
2024-07-27 20:10:35 +0900

    🔨 Add exec.sh

---
+++ chapter10/board/icarus-verilog/exec.sh
@@ -0,0 +1,3 @@
+iverilog -g2012 -o test_top.vvp test_top.sv ../../main/top.sv ../../main/prescaler.sv ../../main/mother_board.sv ../../main/mother_board/cpu.sv ../../main/mother_board/rom.sv \
+&& vvp -N test_top.vvp +vcd \
+&& gtkwave test_top.vcd
